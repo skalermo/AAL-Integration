1a98ea7aa66db0909441980752fb643474d35581
skalermo <skalermo@gmail.com>
2020-01-12 15:01:23 +0100

    Comment functions

--- Graph.py
+++ Graph.py
@@ -2,24 +2,47 @@ class Graph:
     """Graph implementation based on dictionary of sets"""
 
     def __init__(self):
+        """
+        Graph constructor
+        """
         self.adjDict = {}
         self.edgeCount = 0
 
     def __eq__(self, other):
+        """
+        Compare this graph to the other
+        :param other: Compared graph
+        :return: True if graphs are equal
+        """
         if not isinstance(other, Graph):
             # don't attempt to compare against unrelated types
             return NotImplemented
         return self.adjDict == other.getAdjDict() and self.edgeCount == other.getEdgeCount()
 
     def addVertex(self, v):
+        """
+        Add vertex v to graph, if not already added
+        :param v: Vertex to add
+
+        """
         if v not in self.adjDict:
             self.adjDict[v] = []
 
     def addVertices(self, vertices):
+        """
+        Iteratively add vertices from the input list
+        :param vertices: Input list of vertices
+
+        """
         for v in vertices:
             self.addVertex(v)
 
     def removeVertex(self, v):
+        """
+        Remove vertex from graph. Decrease number of edges
+        :param v: Vertex to remove
+
+        """
         if v not in self.adjDict:
             return
 
@@ -29,9 +52,11 @@ class Graph:
         del self.adjDict[v]
 
     def addEdge(self, u, v):
-        """Add edge between u and v vertices
+        """
+        Add edge between u and v vertices
         Do nothing if vertices are the same
-        or if at least one of them doesn't belong to the graph"""
+        or if at least one of them doesn't belong to the graph
+        """
 
         if u == v or \
                 v not in self.adjDict or\
@@ -50,10 +75,21 @@ class Graph:
             self.edgeCount += 1
 
     def addEdges(self, edges):
+        """
+        Iteratively add edges from the input list
+        :param edges: Input list of edges
+
+        """
         for edge in edges:
             self.addEdge(edge[0], edge[1])
 
     def removeEdge(self, v, u):
+        """
+        Remove edge between u and v vertices
+        Do nothing if vertices are the same
+        or if at least one of them doesn't belong to the graph
+
+        """
         if u == v or \
                 v not in self.adjDict or \
                 u not in self.adjDict:
@@ -70,14 +106,29 @@ class Graph:
         return self.edgeCount
 
     def connected(self, u, v):
+        """
+        Check if the edge between vertices u and v exists
+
+        """
         return u in self.adjDict[v]
 
     def getAdjDict(self):
+        """
+        Get dictionary containing vertices
+
+        """
         return self.adjDict
 
     def getDict(self):
+        """
+        Get object's values
+        """
         return self.__dict__
 
     def setDict(self, dict):
+        """
+        Set object's values
+        :param dict: Assign object's values to the new ones from dict
+        """
         self.__dict__ = dict
 
